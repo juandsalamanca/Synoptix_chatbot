87add10ab8b4dbe2da2057a0c6a8aecb76475044
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-03-31 17:53:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from llm_functions import finance_chatbot, get_system_prompt
 st.header("Synoptics")
 
 if "memory" not in st.session_state:
@@ -8,6 +8,10 @@ if "memory" not in st.session_state:
 model = st.selectbox(("Please choose the model to analyze and discuss the documents"), ("gpt-4o", "gpt-4o-mini", "o1"))
 
 theme = st.selectbox(("Please select the type entity to which the documents belong to"), ("University", "Manufacturer"))
+
+if theme and len(st.session_state.memory) == 0:
+  sys_prompt = get_system_prompt(theme)
+  st.session_state.memory.append(sys_prompt)
 
 for i, message in enumerate(st.session_state.memory):
     with st.chat_message(message["role"]):
