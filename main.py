5dc895751e1c5385747631c0d10ec32e903fbe8b
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-03-31 17:47:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import streamlit as st
 
 st.header("Synoptics")
 
+if "memory" not in st.session_state:
+  st.session_state.memory = []
+
+model = st.selectbox(("Please choose the model to analyze and discuss the documents"), ("gpt-4o", "gpt-4o-mini", "o1"))
+
+theme = st.selectbox(("Please select the type entity to which the documents belong to"), ("University", "Manufacturer"))
+
+for i, message in enumerate(st.session_state.memory):
+    with st.chat_message(message["role"]):
+        content = message["content"]
+        if message["role"] == "user":
+            idx = content.index("says:")
+            content = content[idx+6:]
+        st.markdown(content + st.session_state.sensitive_memory[i])
