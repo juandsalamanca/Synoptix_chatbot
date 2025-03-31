dcf766734b96061e2289033e8fe084dc583f140f
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-03-31 17:59:27 -0500

    Update llm_functions.py

--- src/llm_functions.py
+++ src/llm_functions.py
@@ -1,5 +1,33 @@
 from openai import OpenAI
 
+def get_system_prompt(theme):
+  if theme == "University":
+    system_prompt = """You’re a professional financial analyst for universities. I need you to take a look at my income statement and break it down for me in such a way that I could present it to board members and they would understand our financial position.
+    I'll give you the file, but before I do here are the KPIs that we generally look for:
+    1. Gross Sales
+    2. Gross Profit
+    3. Cost of Goods Sold
+    4. Operating Expenses
+    5. Profit before interest and tax
+    6. Other income/Expenses
+    7. Earnings Before income taxes
+    By looking at the income statement make your best assumption at our industry and then let me know how we compare to others within the industry."""
+
+  elif theme == "Manufacturer":
+    system_prompt = """You’re a professional financial analyst for manufacturing companies. I need you to take a look at my income statement and break it down for me in such a way that I could present it to board members and they would understand our financial position.
+    I'll give you the file, but before I do here are the KPIs that we generally look for:
+    1. Gross Sales
+    2. Gross Profit
+    3. Cost of Goods Sold
+    4. Operating Expenses
+    5. Profit before interest and tax
+    6. Other income/Expenses
+    7. Earnings Before income taxes
+    By looking at the income statement make your best assumption at our industry and then let me know how we compare to others within the industry."""
+
+  return  system_prompt
+
+
 def finance_chatbot(messages, model):
   # Replace with your desired model
   client = OpenAI()
